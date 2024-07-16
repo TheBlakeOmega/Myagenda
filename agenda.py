159623d3d8101c89525de076efc186e711a8159b
Belvedere <vincenzo.belvedere@bip-group.com>
2024-07-16 17:17:21 +0200

    first comm

--- agenda.py
+++ agenda.py
@@ -1,18 +1,32 @@
 class Event:
     def __init__(self, title, date, description):
+        self.title = title
+        self.date = date
+        self.description = description
 
     def __str__(self):
+        return "[ " + self.date + ": " + self.title + " (" + self.description + ")]"
 
 
 class Agenda:
     def __init__(self):
-
+        self.EventDict = dict()
 
     def add_event(self, title, date, description):
+        self.EventDict[title] = Event(title, date, description)      
+        print("\n\n")
 
     def remove_event(self, title):
+        if title in self.EventDict.keys():
+            del self.EventDict[title]
+        else:
+            print("Event not found")
+        print("\n\n")
 
     def list_events(self):
+        for key in self.EventDict.keys():
+            print(self.EventDict[key])
+        print("\n\n")
 
 
 if __name__ == "__main__":
@@ -40,8 +54,15 @@ if __name__ == "__main__":
         choice = input("Enter your choice: ")
 
         if choice == "1":
+            title = input("Enter your title: ")
+            date = input("Enter your date: ")
+            description = input("Enter your description: ")
+            agenda.add_event(title,date,description)
         elif choice == "2":
+            title = input("Enter your title to delete: ")
+            agenda.remove_event(title)
         elif choice == "3":
+            agenda.list_events()
         elif choice == "4":
             break
         else:
--- test_agenda.py
+++ test_agenda.py
@@ -7,10 +7,14 @@ class TestAgenda(unittest.TestCase):
         self.agenda = Agenda()
 
     def test_add_event(self):
+        self.agenda.add_event("AAA","BBB","CCC")
+        self.assertEqual(len(self.agenda.EventDict), 1)
+        self.assertEqual(str(self.agenda.EventDict["AAA"]), "[ BBB: AAA (CCC)]")
+    
 
     def test_remove_event(self):
-
-    def test_list_events(self):
+        self.agenda.remove_event("AAA")
+        self.assertEqual(len(self.agenda.EventDict), 0)
 
 
 if __name__ == "__main__":
